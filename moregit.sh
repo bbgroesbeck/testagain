4484de92a8e242a136c260a5bbdfe1454e861ca6
Anja Schmidt <anjaschmidt@Anjas-MacBook-Air.local>
2022-01-06 10:02:32 -0700

    Made new branch

--- moregit.sh
+++ moregit.sh
@@ -1,8 +1,1 @@
-#this is where Bryce started
-#this is tyler
-#watch red letter media
-echo "talmage's code"
-#hey there
-#Noah Was here
-#Mckay is tired
-# Anja got there eventually
+# This is all my branch.
