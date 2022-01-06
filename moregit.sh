0b75346a13291acb69eefdf9f6321d13ee74c1c9
Anja Schmidt <anjaschmidt@Anjas-MacBook-Air.local>
2022-01-06 10:15:35 -0700

    Trying to fix main branch mistake

--- moregit.sh
+++ moregit.sh
@@ -1,1 +1,8 @@
-# This is all my branch.
+#this is where Bryce started
+#this is tyler
+#watch red letter media
+echo "talmage's code"
+#hey there
+#Noah Was here
+#Mckay is tired
+# Anja got there eventually
