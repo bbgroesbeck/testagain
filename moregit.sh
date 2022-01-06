ec2c5da9fc8dea8b1e71161cc1b72939684c349b
admin <admin@admins-MacBook-Air.local>
2022-01-06 09:43:25 -0700

    please work

--- moregit.sh
+++ moregit.sh
@@ -1,13 +1,7 @@
 #this is where Bryce started
-<<<<<<< HEAD
-this is tyler
-
-echo "talmage's code"
-# McKay has struggled pushing up code
-=======
 #this is tyler
 #watch red letter media
 echo "talmage's code"
 #hey there
 #Noah Was here
->>>>>>> 553e280cd8a94980c07919cfeedbed22a92cd200
+#Mckay is tired
