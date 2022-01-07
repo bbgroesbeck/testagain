e25d7d8eb52630eb43a20548194d59902e758d62
Mountainland Tech <mtech@Mountainlands-Mac-mini.local>
2022-01-07 08:42:01 -0700

    On Fri Jan  7 08:42:01 MST 2022 mtech score was 0

--- mathFun.sh
+++ mathFun.sh
@@ -21,6 +21,13 @@ function addition() {
         echo "incorrect"
         total=${#score[@]}
         echo "Final Score : ${total}"
+        # git checkout main
+        # git pull
+        git checkout feature/tyler
+        # git merge main
+        git add mathFun.sh
+        git commit -m "On $(date) $(whoami) score was ${total}"
+        git push
         exit 1
     fi
 
@@ -52,3 +59,4 @@ add)
     #   STATEMENTS
     #   ;;
 esac
+
