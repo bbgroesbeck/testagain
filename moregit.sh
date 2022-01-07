c3b38a058bedb40db936045330f94b888b517cf3
Mountainland Tech <mtech@Mountainlands-Mac-mini.local>
2022-01-07 07:47:54 -0700

    MoreGit

--- moregit.sh
+++ moregit.sh
@@ -4,7 +4,12 @@
 echo "talmage's code"
 #hey there
 #Noah Was here
+<<<<<<< HEAD
 git checkout main
 git pull
 git checkout -b Noah
 git merge main your code
+=======
+#Mckay is tired
+# Anja got there eventually
+>>>>>>> main
