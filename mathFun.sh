e061d934aecf331f7d65c46c9238b14b24afd799
Noah Coleman <noahcoleman@noahs-mbp-2.lan>
2022-01-07 13:35:40 -0700

    New commit

--- mathFun.sh
+++ mathFun.sh
@@ -21,13 +21,6 @@ function addition() {
         echo "incorrect"
         total=${#score[@]}
         echo "Final Score : ${total}"
-        # git checkout main
-        # git pull
-        git checkout noah
-        # git merge main
-        git add mathFun.sh
-        git commit -m "On $(date) Noah's score was ${total}"
-        git push
         exit 1
     fi
 
