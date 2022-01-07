64a204afded1ed95c811de1a876e0d5b807e7526
zealantanner <zealan.tanner0869@stu.mtec.edu>
2022-01-07 08:49:37 -0700

    On Fri Jan  7 08:49:37 MST 2022 hyrumtanner score was 0

--- mathFun.sh
+++ mathFun.sh
@@ -23,7 +23,7 @@ function addition() {
         echo "Final Score : ${total}"
         # git checkout main
         # git pull
-        git checkout feature/anja
+        git checkout feature/Zealan
         # git merge main
         git add mathFun.sh
         git commit -m "On $(date) $(whoami) score was ${total}"
